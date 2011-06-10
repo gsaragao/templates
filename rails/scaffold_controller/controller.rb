d4f5a0d2400f02a8d02479c21769a8a6ceceb10e
Gustavo Aragao <gustavo@couggi.com>
2011-06-10 11:29:35 -0300

    first commit

---
+++ rails/scaffold_controller/controller.rb
@@ -0,0 +1,54 @@
+# encoding : utf-8
+class <%= controller_class_name %>Controller < ApplicationController
+
+respond_to :html
+
+  def index
+    @<%= plural_table_name %> = <%= orm_class.all(class_name) %>
+    respond_with @<%= plural_table_name %>
+  end
+
+  def show
+    @<%= singular_table_name %> = <%= orm_class.find(class_name, "params[:id]") %>
+    respond_with @<%= singular_table_name %>
+  end
+
+  def new
+    @<%= singular_table_name %> = <%= orm_class.build(class_name) %>
+    respond_with @<%= singular_table_name %>
+  end
+
+  def edit
+    @<%= singular_table_name %> = <%= orm_class.find(class_name, "params[:id]") %>
+  end
+
+  def create
+    @<%= singular_table_name %> = <%= orm_class.build(class_name, "params[:#{singular_table_name}]") %>
+    
+    if @<%= orm_instance.save %>
+      flash[:notice] = "A <%= singular_table_name %> '#{@<%= singular_table_name %>.descricao}' foi criada com sucesso."
+      redirect_to <%= plural_table_name %>_path
+    else
+      render :action => :new 
+    end
+     
+  end
+
+  def update
+    @<%= singular_table_name %> = <%= orm_class.find(class_name, "params[:id]") %>
+
+    if @<%= singular_table_name %>.update_attributes(params[:<%= singular_table_name %>])
+      flash[:notice] = "A <%= singular_table_name %> '#{@<%= singular_table_name %>.descricao}' foi atualizado com sucesso."
+      redirect_to <%= plural_table_name %>_path
+    else
+      render :action => :edit
+    end
+end
+
+  def destroy
+    @<%= singular_table_name %> = <%= orm_class.find(class_name, "params[:id]") %>
+    @<%= orm_instance.destroy %>
+    flash[:notice] = "A <%= singular_table_name %> '#{@<%= singular_table_name %>.descricao}' foi excluída com sucesso."
+    redirect_to <%= plural_table_name %>_path
+  end
+end
