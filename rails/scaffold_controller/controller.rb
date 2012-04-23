0b3ba7586ac00064d245d7490f3ac61bc6dc1ab5
Gustavo Aragao <gustavo@couggi.com>
2012-04-23 20:13:52 -0300

    Atualizacao do template

--- rails/scaffold_controller/controller.rb
+++ rails/scaffold_controller/controller.rb
@@ -2,26 +2,28 @@
 class <%= controller_class_name %>Controller < ApplicationController
 
   respond_to :html
-  layout "application", :except  => [:show]
   before_filter :setar_classe_menu
-  before_filter :carrega_<%= singular_table_name %>, :only  => [:show, :edit, :update, :destroy]
+  before_filter :manage_params, :only => [:index]
+  before_filter :load_<%= singular_table_name %> , :only => [:show, :edit, :update, :destroy]
 
   def index
-     @<%= singular_table_name %> = <%= orm_class.build(class_name, "params[:#{singular_table_name}]") %>
-     @<%= plural_table_name %> = <%= singular_table_name.capitalize %>.pesquisa(@<%= singular_table_name %>.descricao)
+    @<%= singular_table_name %> = <%= orm_class.build(class_name, "params[:#{singular_table_name}]") %>
+    @<%= plural_table_name %> = <%= singular_table_name.capitalize %>.pesquisar(params[:<%= singular_table_name %>],params[:page])
     respond_with @<%= plural_table_name %>
-  end 
+  end
 
   def show
     respond_with @<%= singular_table_name %>
   end
 
   def new
+    load_combos
     @<%= singular_table_name %> = <%= orm_class.build(class_name) %>
     respond_with @<%= singular_table_name %>
   end
 
   def edit
+    load_combos
   end
 
   def create
@@ -31,8 +33,10 @@ class <%= controller_class_name %>Controller < ApplicationController
       flash[:notice] = t('msg.create_sucess')
       redirect_to <%= plural_table_name %>_path
     else
+      load_combos
       render :action => :new 
     end
+     
   end
 
   def update
@@ -40,9 +44,10 @@ class <%= controller_class_name %>Controller < ApplicationController
       flash[:notice] = t('msg.update_sucess')
       redirect_to <%= plural_table_name %>_path
     else
+      load_combos
       render :action => :edit
     end
- end
+  end
 
   def destroy
     @<%= orm_instance.destroy %>
@@ -51,13 +56,23 @@ class <%= controller_class_name %>Controller < ApplicationController
   end
   
   private
-
+  
   def setar_classe_menu
-    @class_<%= singular_table_name %> = 'selected'  
+    @class_<%= singular_table_name %> = 'active'  
   end
   
-  def carrega_<%= singular_table_name %>
-    @<%= singular_table_name %> = <%= singular_table_name.capitalize %>.find(params[:id])
+  def load_<%= singular_table_name %>
+    @<%= singular_table_name %> = <%= orm_class.find(class_name, "params[:id]") %>
+  end
+  
+  def load_combos 
+    #Collections
+  end
+  
+  def manage_params
+    if (!params[:<%= singular_table_name %>].nil?) 
+       params[:<%= singular_table_name %>].delete_if{|k,v| v.blank?}
+    end
   end
   
 end
